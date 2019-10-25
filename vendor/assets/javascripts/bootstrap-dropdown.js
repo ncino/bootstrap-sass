2d32ddd9916d504b300ad3fd8f07bcba5bf42433
Bryan Müller <bmuller@simplenexus.com>
2019-10-25 12:58:58 -0600

    fix(Dropdown): Fix XSS in dropdown

--- vendor/assets/javascripts/bootstrap-dropdown.js
+++ vendor/assets/javascripts/bootstrap-dropdown.js
@@ -123,7 +123,7 @@
       selector = selector && /#/.test(selector) && selector.replace(/.*(?=#[^\s]*$)/, '') //strip for ie7
     }
 
-    $parent = selector && $(selector)
+    $parent = selector && $(document).find(selector)
 
     if (!$parent || !$parent.length) $parent = $this.parent()
 
