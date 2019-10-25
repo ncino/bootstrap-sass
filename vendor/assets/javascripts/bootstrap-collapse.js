7f354e8c208d147f9593a213cc9fca295c6d3746
Bryan Müller <bmuller@simplenexus.com>
2019-10-25 12:58:14 -0600

    fix(Collapse): Fix XSS in collapse

--- vendor/assets/javascripts/bootstrap-collapse.js
+++ vendor/assets/javascripts/bootstrap-collapse.js
@@ -159,7 +159,7 @@
       , target = $this.attr('data-target')
         || e.preventDefault()
         || (href = $this.attr('href')) && href.replace(/.*(?=#[^\s]+$)/, '') //strip for ie7
-      , option = $(target).data('collapse') ? 'toggle' : $this.data()
+      , option = $(document).find(target).data('collapse') ? 'toggle' : $this.data()
     $this[$(target).hasClass('in') ? 'addClass' : 'removeClass']('collapsed')
     $(target).collapse(option)
   })
